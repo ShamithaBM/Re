5bdeb943a20a8e6466bf6895881bc1c3082b5d5a
HP-1 <HP-1@HP>
2021-06-24 19:42:46 +0530

    2nd module

---
+++ Re/src/w1.java
@@ -0,0 +1,14 @@
+public class w1 {
+
+	public static void main(String[] args) {
+		int n=110;
+		if(n>0)
+			System.out.println("its +ve");
+		else if(n<0)
+			System.out.println("its -ve");
+		else
+			System.out.println("its 0");
+		
+	}
+
+}
